0e5f16477660b6c909b6efcf24348649cb24de46
Sam Ay <30203976+sam3ay@users.noreply.github.com>
2023-07-01 20:12:35 +0000

    code server added dotnet install

--- scripts/aws-sagemaker-studio-lab-setup.sh
+++ scripts/aws-sagemaker-studio-lab-setup.sh
@@ -6,19 +6,28 @@ set -eux
 #  VARIABLES  #
 ###############
 
-CODE_SERVER_DOWNLOAD_URL="https://github.com/aws-samples/amazon-sagemaker-codeserver/releases/download/v0.1.5/amazon-sagemaker-codeserver-0.1.5.tar.gz"
-# String pattern maching to isolate folder name
-CODE_SERVER_ARCHIVE=${CODE_SERVER_DOWNLOAD_URL##*/}
+# CODE_SERVER_DOWNLOAD_URL="https://github.com/aws-samples/amazon-sagemaker-codeserver/releases/download/v0.1.5/amazon-sagemaker-codeserver-0.1.5.tar.gz"
+# # String pattern maching to isolate folder name
+# CODE_SERVER_ARCHIVE=${CODE_SERVER_DOWNLOAD_URL}
 
-#############
-#  INSTALL  #
-#############
+# #############
+# #  INSTALL  #
+# #############
+# # Download and extract codeserver files
+# curl -LO ${CODE_SERVER_DOWNLOAD_URL}
+# tar -xvzf ${CODE_SERVER_ARCHIVE}
 
-curl -LO ${CODE_SERVER_DOWNLOAD_URL}
-tar -xvzf ${CODE_SERVER_ARCHIVE}
+# # install code server files
+# cd ${CODE_SERVER_ARCHIVE%.tar*}/install-scripts/studio
+# chmod +x install-codeserver.sh
+# ./install-codeserver.sh
 
-cd ${CODE_SERVER_ARCHIVE%.tar*}/install-scripts/studio
- 
-chmod +x install-codeserver.sh
-./install-codeserver.sh
+#Install codeserver
+curl -sSL https://raw.githubusercontent.com/aws-samples/amazon-sagemaker-codeserver/main/install-scripts/studio/install-codeserver.sh | bash /dev/stdin
+
+# Install dotnet LTS
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel LTS
+
+
+
 
